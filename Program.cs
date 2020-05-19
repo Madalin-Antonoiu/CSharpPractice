0c641932ecccc573918b17fb87d4e45de839e224
AntonoiuM <AntonoiuM@CSI-00522>
2020-05-19 20:46:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello ma");
+            string message = Console.ReadLine();
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+        }
+    }
+}
